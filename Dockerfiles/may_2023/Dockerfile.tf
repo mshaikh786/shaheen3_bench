ed693ccfb0d991609cf6d7da98dd35c8d7f58596
Mohsin A Shaikh <mohsinshaikh786@gmail.com>
2023-05-27 17:03:50 +0300

    Fixing driver issue in TF image and Triton for arm issue in Torch image

--- Dockerfiles/may_2023/Dockerfile.tf
+++ Dockerfiles/may_2023/Dockerfile.tf
@@ -1,8 +1,13 @@
-FROM nvcr.io/nvidia/tensorflow:23.04-tf2-py3 AS builder
+FROM nvcr.io/nvidia/tensorflow:22.12-tf2-py3 AS builder
+#FROM nvcr.io/nvidia/tensorflow:23.04-tf2-py3 AS builder
 WORKDIR /software
 #RUN git clone https://github.com/SilvioGiancola/SoccerNetv2-DevKit.git \
-RUN  pip install  --prefix=/software SoccerNet==0.1.8 opencv-python==3.4.11.45 imutils moviepy \
-&&  mkdir /data
+RUN  pip install -v --prefix=/software --verbose SoccerNet==0.1.8 
+RUN  pip install -v --prefix=/software --verbose opencv-python
+#==3.4.11.45
+RUN  pip install -v --prefix=/software --verbose imutils 
+RUN  pip install -v --prefix=/software --verbose moviepy 
+RUN  mkdir /data
 ENV PYTHONPATH=/software/lib/python3.8/site-packages:${PYTHONPATH}
 ENV SOCCERNET_INSTALL_DIR=/software
 COPY download_data.py /data/download_data.py
